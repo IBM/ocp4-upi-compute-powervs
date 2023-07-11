808a91a77a84c3af8e5bd21ebafbbdc695235c36
Paul Bastide <pbastide@us.ibm.com>
2023-07-11 15:33:55 -0400

    Cleanup formatting for files in checks
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/0_checks/versions.tf
+++ modules/0_checks/versions.tf
@@ -4,7 +4,7 @@
 ################################################################
 
 terraform {
-    required_providers {
+  required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
       version = "~> 1.55.0"
