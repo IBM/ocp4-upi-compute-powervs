5c0b6e1e098fff48b93d7d7ff03f90c753d29849
Paul Bastide <pbastide@us.ibm.com>
2023-07-16 20:26:21 -0400

    fix: harden transit gateway
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/1_vpc_support/transit_gateway.tf
+++ modules/1_vpc_support/transit_gateway.tf
@@ -3,17 +3,25 @@
 # SPDX-License-Identifier: Apache-2.0
 ################################################################
 
+data "ibm_tg_gateways" "mac_tg_gws" {
+}
+
+locals {
+  tg = [for x in data.ibm_tg_gateways.mac_tg_gws.transit_gateways : x if x.name == "${var.vpc_name}-tg"]
+}
+
 # Ref: https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/resources/tg_gateway
 resource "ibm_tg_gateway" "mac_tg_gw" {
+  count          = local.tg == [] ? 1 : 0
   name           = "${var.vpc_name}-tg"
   location       = var.vpc_region
   global         = true
   resource_group = data.ibm_is_vpc.vpc.resource_group
 }
 
-# In the case where it previous exists
 data "ibm_tg_gateway" "mac_tg_gw_check" {
-  name = "${var.vpc_name}-tg"
+  depends_on = [ibm_tg_gateway.mac_tg_gw]
+  name       = "${var.vpc_name}-tg"
 }
 
 locals {
@@ -23,7 +31,7 @@ locals {
 # Ref: https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/resources/tg_connection
 resource "ibm_tg_connection" "vpc_tg_connection" {
   count        = local.tg_conn == [] ? 1 : 0
-  gateway      = ibm_tg_gateway.mac_tg_gw.id
+  gateway      = local.tg == [] ? ibm_tg_gateway.mac_tg_gw[0].id : local.tg.id
   network_type = "vpc"
   name         = "${var.vpc_name}-vpc-conn"
   network_id   = data.ibm_is_vpc.vpc.resource_crn
