4ab8e682ac3aaa343922fdf01a1909d6e80fed3b
Paul Bastide <pbastide@us.ibm.com>
2023-07-27 14:49:48 -0400

    feat(scripts): import-centos image has a supporting script
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

---
+++ scripts/import-centos.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+################################################################
+# Copyright 2023 - IBM Corporation. All rights reserved
+# SPDX-License-Identifier: Apache-2.0
+################################################################
+
+# Helper file to import the centos image using the ibmcloud cli
+
+ibmcloud plugin install power-iaas -f
+
+POWERVS_CRN=$(ibmcloud pi sl 2>&1 | grep ${POWERVS_SERVICE_INSTANCE_ID} | awk '{print $1}')
+ibmcloud pi st "${POWERVS_CRN}"
+
+ibmcloud pi image-create CentOS-Stream-8
+echo "Finished importing CentOS-Stream-8
