fac42ca5181c3e490c93d10a0bfcaab6f6995782
Paul Bastide <pbastide@us.ibm.com>
2023-07-31 16:04:48 -0400

    fix formatting
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/2_pvs_prepare/images/images.tf
+++ modules/2_pvs_prepare/images/images.tf
@@ -10,7 +10,7 @@ data "ibm_pi_catalog_images" "catalog_images" {
 }
 
 locals {
-  catalog_bastion_image = [for x in data.ibm_pi_catalog_images.catalog_images.images : x if x.name == var.rhel_image_name || x.name == "rhcos-${var.rhcos_import_image_storage_type}-image" ]
+  catalog_bastion_image = [for x in data.ibm_pi_catalog_images.catalog_images.images : x if x.name == var.rhel_image_name || x.name == "rhcos-${var.rhcos_import_image_storage_type}-image"]
   bastion_image_id      = length(local.catalog_bastion_image) == 0 ? data.ibm_pi_image.bastion[0].id : local.catalog_bastion_image[0].image_id
   bastion_storage_pool  = length(local.catalog_bastion_image) == 0 ? data.ibm_pi_image.bastion[0].storage_pool : local.catalog_bastion_image[0].storage_pool
 }
