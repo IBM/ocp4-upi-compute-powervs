1fd538eb8d35e66e73e020649b417ab766de5ffd
Paul Bastide <pbastide@us.ibm.com>
2023-08-10 21:32:49 -0400

    fix: worker ignition considers the interface status
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/5_worker/templates/worker.sh
+++ modules/5_worker/templates/worker.sh
@@ -10,11 +10,15 @@
 # v1: tx-checksumming off / restarts wait-online
 # Content: IyEvYmluL2Jhc2gKaWYgWyAiIiA9ICJlbnYzIiBdICYmIFsgIiIgPSAidXAiIF0KdGhlbgogIC9zYmluL2V0aHRvb2wgLS1vZmZsb2FkIGVudjMgdHgtY2hlY2tzdW1taW5nIG9mZgogIHN5c3RlbWN0bCByZXN0YXJ0IE5ldHdvcmtNYW5hZ2VyLXdhaXQtb25saW5lLnNlcnZpY2UgCmZpCg==
 
+#v2: missing details for interface and action 
+# Content: aWYgWyAiIiA9ICJlbnYzIiBdICYmIFsgIiIgPSAidXAiIF0KdGhlbgogIC9zYmluL2V0aHRvb2wgLS1vZmZsb2FkIGVudjMgdHgtY2hlY2tzdW1taW5nIG9mZgogIHN5c3RlbWN0bCByZXN0YXJ0IE5ldHdvcmtNYW5hZ2VyLXdhaXQtb25saW5lLnNlcnZpY2UgCmVsc2UgCiAgImVjaG8gIm5vdCBydW5uaW5nIHR4LWNoZWNrc3VtbWluZyBvZmYiCmZpCg==
 cat << EOF | base64 --wrap=0
-if [ "" = "env3" ] && [ "" = "up" ]
+if [ "$1" = "env3" ] && [ "$2" = "up" ]
 then
   /sbin/ethtool --offload env3 tx-checksumming off
+  systemctl restart NetworkManager-wait-online.service 
+else 
+  "echo "not running tx-checksumming off"
 fi
-systemctl restart NetworkManager-wait-online.service 
 EOF
 echo ""
