73b8d4a3e93a5f7c5b699deef46187444548f711
Paul Bastide <pbastide@us.ibm.com>
2023-08-16 16:01:22 -0400

    fix: add connection refresh to dhcp service creation
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/2_pvs_prepare/network/network.tf
+++ modules/2_pvs_prepare/network/network.tf
@@ -38,10 +38,16 @@ resource "time_sleep" "wait_dhcp" {
   destroy_duration = "120s"
 }
 
+data "ibm_pi_cloud_connection" "cloud_connection" {
+  depends_on               = [time_sleep.wait_dhcp]
+  pi_cloud_instance_id     = var.powervs_service_instance_id
+  pi_cloud_connection_name = ibm_pi_cloud_connection.new_cloud_connection[0].pi_cloud_connection_name
+}
+
 resource "ibm_pi_dhcp" "new_dhcp_service" {
-  depends_on             = [time_sleep.wait_dhcp]
+  depends_on             = [data.ibm_pi_cloud_connection.cloud_connection]
   pi_cloud_instance_id   = var.powervs_service_instance_id
-  pi_cloud_connection_id = ibm_pi_cloud_connection.new_cloud_connection[0].id
+  pi_cloud_connection_id = data.ibm_pi_cloud_connection.cloud_connection.id
   pi_cidr                = var.powervs_machine_cidr
   pi_dns_server          = var.vpc_support_server_ip
   pi_dhcp_snat_enabled   = var.enable_snat
