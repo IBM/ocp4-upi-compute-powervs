ebbbb95197fecc5bc61dc95f5819526f3465c59b
Paul Bastide <pbastide@us.ibm.com>
2023-09-19 14:00:50 -0400

    update create-var-file to reflect the cicd preferred fixed network
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- scripts/create-var-file.sh
+++ scripts/create-var-file.sh
@@ -189,6 +189,7 @@ mac_tags = [ "mac-cicd" ]
 
 #override_network_name="DHCPSERVERmac-dhcp-${VPC_REGION}_Private"
 #override_transit_gateway_name="${OVERRIDE_PREFIX}-tg"
+use_fixed_network=true
 EOFXEOF
 }
 
