496df1c9d320cd270fec28ae23b0456c75a0f986
Paul Bastide <pbastide@us.ibm.com>
2023-10-30 12:55:07 -0400

    chore: update version for terraform-provider-ibm from 1.58.0 to 1.59.0
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/5_worker/versions.tf
+++ modules/5_worker/versions.tf
@@ -15,7 +15,7 @@ terraform {
     }
     ibm = {
       source                = "IBM-Cloud/ibm"
-      version               = "~> 1.58.0"
+      version               = "~> 1.59.0"
       configuration_aliases = [ibm]
     }
     http = {
