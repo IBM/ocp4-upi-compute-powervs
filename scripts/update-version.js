d72581579fa23c0325e8e45eeb809a50c78ff08b
Paul Bastide <pbastide@us.ibm.com>
2024-01-05 12:16:16 -0500

    updated scripts
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

---
+++ scripts/update-version.js
@@ -0,0 +1,27 @@
+// ################################################################
+// # Copyright 2023 - IBM Corporation. All rights reserved
+// # SPDX-Libmcloudense-Identifier: Apache-2.0
+// ################################################################
+
+const fs = require('node:fs');
+const readline = require('readline');
+const filename = process.argv[2];
+
+const data = fs.readFileSync(filename, 'utf8');
+const lines = data.split("\n");
+
+var ibmcloud = false
+for (const line of lines) {
+	if (ibmcloud) {
+		ibmcloud = false
+		//Modify this line
+		const idx = line.indexOf("\"");
+		console.log(line.substring(0, idx) + "\"~> 1.61.0\"");
+	} else {
+		if (line.includes("IBM-Cloud/ibm")) {
+			ibmcloud = true
+			console.log("Found")
+		}
+		console.log(line);
+	}
+}
---
+++ scripts/update-version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+################################################################
+# Copyright 2023 - IBM Corporation. All rights reserved
+# SPDX-License-Identifier: Apache-2.0
+################################################################
+
+for FILE in $(find $(pwd)/../ -iname 'versions.tf' -exec grep -rn ibm {} \; | awk -F ':' '{print $1}' | sort -u)
+do
+echo "FILE: ${FILE}"
+node update-version.js ${FILE} > ${FILE}
+done
