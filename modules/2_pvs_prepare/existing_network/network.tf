abab643e433349a3094a29311023c13857259ecf
Paul Bastide <pbastide@us.ibm.com>
2024-01-08 13:46:38 -0500

    fix: formatting was incorrect in existing_network
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/2_pvs_prepare/existing_network/network.tf
+++ modules/2_pvs_prepare/existing_network/network.tf
@@ -10,7 +10,7 @@ resource "ibm_pi_network" "bastion_public_network" {
   pi_network_name      = "${var.name_prefix}-pub-net"
   pi_cloud_instance_id = var.powervs_service_instance_id
   pi_network_type      = "pub-vlan"
-  pi_network_mtu = 9000
+  pi_network_mtu       = 9000
   # Dev Note: There appears to be an issue when 2 dns providers are passed in.connection {
   # Opting to leave commented out for now, as it is implicitly using 9.9.9.9
 }
