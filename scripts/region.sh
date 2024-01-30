f32de49ff726dc223c1072377c4535b4cf3a64c1
Paul Bastide <pbastide@us.ibm.com>
2024-01-30 11:52:22 -0500

    OCTOPUS-519: update scripts region.sh
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- scripts/region.sh
+++ scripts/region.sh
@@ -7,6 +7,7 @@
 
 # Maps or overrides the regions
 
+# v3: update for new regions
 # v2: reverted the mapping for jp-osa to osa21
 # v1: jp-osa: osa21 is remapped to tok04
 
@@ -14,12 +15,16 @@ REGION="$1"
 OVERRIDE="$2"
 
 case "$REGION" in
-  ("us-south") echo "dal10" ;;
-  ("jp-osa") echo "osa21" ;;
-  ("eu-gb") echo "lon05" ;;
-  ("ca-tor") echo "mon01" ;;
-  ("br-sao") echo "sao01" ;;
+  ("us-south") echo "dal10";;
+  ("us-east") echo "wdc06";;
+  ("br-sao") echo "sao01";;
+  ("ca-tor") echo "tor01";;
+  ("ca-mon") echo "mon01";;
+  ("eu-de") echo "eu-de-1";;
+  ("eu-gb") echo "lon06";;
+  ("eu-es") echo "mad02";;
   ("au-syd") echo "syd05" ;;
   ("jp-tok") echo "tok04" ;;
+  ("jp-osa") echo "osa21";;
   (*) echo "$REGION" ;;
 esac
