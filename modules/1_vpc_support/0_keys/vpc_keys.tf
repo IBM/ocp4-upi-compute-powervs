43f06b269e549ead32cd8b4b1f69a8b7637b435f
Paul Bastide <paul@bastide.org>
2024-04-25 07:52:56 -0400

    Update modules/1_vpc_support/0_keys/vpc_keys.tf
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/1_vpc_support/0_keys/vpc_keys.tf
+++ modules/1_vpc_support/0_keys/vpc_keys.tf
@@ -18,7 +18,6 @@ locals {
 }
 
 resource "ibm_is_ssh_key" "vpc_support_ssh_key_cond_create" {
-  #  count          = local.keys == [] ? 1 : 0
   count          = var.skip_vpc_key ? 0 : 1
   name           = "${var.vpc_name}-keypair"
   public_key     = local.public_key
