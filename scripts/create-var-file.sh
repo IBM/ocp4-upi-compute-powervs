dc0e8727c2e963a18d27b45bfe6c3fc5ac014efc
Paul Bastide <pbastide@us.ibm.com>
2024-12-01 20:15:45 -0500

    chore: move to e980 for the create-var-file.sh
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- scripts/create-var-file.sh
+++ scripts/create-var-file.sh
@@ -162,10 +162,10 @@ ${IBMCLOUD} is key-create cicd-key @data/id_rsa.pub  || true
 # Set the Machine Type
 if [[ "${POWERVS_REGION}" == "wdc06" ]]
 then
-    MACHINE_TYPE="s922"
+    MACHINE_TYPE="e980"
 else
     # Default (original s1022)
-    MACHINE_TYPE="s922"
+    MACHINE_TYPE="e980"
 fi
 echo "MACHINE_TYPE=${MACHINE_TYPE}"
 
