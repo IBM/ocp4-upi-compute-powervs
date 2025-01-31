007af2da343ddfc7a4d4e2271723e35b7b35ddb8
Paul Bastide <pbastide@us.ibm.com>
2025-01-31 14:31:55 -0500

    OpenShiftP-247: refine the output
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/1_vpc_support/find_lbs.tf
+++ modules/1_vpc_support/find_lbs.tf
@@ -12,41 +12,41 @@ data "ibm_resource_group" "group" {
 
 # only get the subnets in the vpc
 data "ibm_is_subnets" "subnets" {
-  vpc = data.ibm_is_vpc.vpc.id
+  vpc            = data.ibm_is_vpc.vpc.id
   resource_group = data.ibm_resource_group.group.id
 }
 
 # gets all lbs in the region
 data "ibm_is_lbs" "lbs" {
-    // Empty
+  // Empty
 }
 
 locals {
-    subnets_used_in_vpc = [for sn in data.ibm_is_subnets.subnets.subnets: sn.id]
+  subnets_used_in_vpc = [for sn in data.ibm_is_subnets.subnets.subnets : sn.id]
 
-    // Load Balancer Id
-    subnets_from_lbs_with_ips = flatten([
-        for lb in data.ibm_is_lbs.lbs.load_balancers: 
-            flatten([
-                for sn in lb.subnets[*]: 
-                    flatten([
-                        for snx in data.ibm_is_subnets.subnets.subnets:
-                            sn.id == snx.id && snx.vpc == data.ibm_is_vpc.vpc.id && length(lb.private_ip) > 0 ?
-                                [{
-                                    id = lb.id
-                                    lb_name = lb.name
-                                    sn = sn
-                                    vpc = data.ibm_is_vpc.vpc.id
-                                    private_ip = lb.private_ip[*].address
-                                }]
-                                : []
-                    ])
-            ])
+  // Load Balancer Id
+  subnets_from_lbs_with_ips = flatten([
+    for lb in data.ibm_is_lbs.lbs.load_balancers :
+    flatten([
+      for sn in lb.subnets[*] :
+      flatten([
+        for snx in data.ibm_is_subnets.subnets.subnets :
+        sn.id == snx.id && snx.vpc == data.ibm_is_vpc.vpc.id && length(lb.private_ip) > 0 ?
+        [{
+          id         = lb.id
+          lb_name    = lb.name
+          sn         = sn
+          vpc        = data.ibm_is_vpc.vpc.id
+          private_ip = lb.private_ip[*].address
+        }]
+        : []
+      ])
     ])
+  ])
 
-    load_balancer_ips = distinct(flatten([for slip in local.subnets_from_lbs_with_ips:
-            !strcontains(slip.lb_name, "-api-")?
-            flatten(
-                [for pip in slip.private_ip: pip]):
-            []]))
+  load_balancer_ips = distinct(flatten([for slip in local.subnets_from_lbs_with_ips :
+    !strcontains(slip.lb_name, "-api-") ?
+    flatten(
+    [for pip in slip.private_ip : pip]) :
+  []]))
 }
--- modules/1_vpc_support/outputs.tf
+++ modules/1_vpc_support/outputs.tf
@@ -27,7 +27,7 @@ output "transit_gateway_status" {
   value = !var.setup_transit_gateway ? module.existing_gateway[0].existing_tg_status : module.transit_gateway[0].new_tg_status
 }
 
-// Highly unlikely this is empty or zero, best to throw an error
+// Output could be more than one 
 output "load_balancer_ips" {
-  value = load_balancer_ips[0]
+  value = local.load_balancer_ips
 }
