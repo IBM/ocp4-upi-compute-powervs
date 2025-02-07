87ea4ab5b7e984dc00cb2a276895aa11ef5ca42c
Paul Bastide <pbastide@us.ibm.com>
2025-02-06 21:51:33 -0500

    fix: ox2-2x16 is a storage optimized profile
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/1_vpc_support/4_vsi/vsi.tf
+++ modules/1_vpc_support/4_vsi/vsi.tf
@@ -19,9 +19,9 @@ resource "ibm_is_instance" "supp_vm_vsi" {
   zone    = var.zone
   keys    = [var.key_id]
   image   = data.ibm_is_image.supp_vm_image[0].id
-  profile = "cx2d-4x8"
+  profile = "ox2-2x16"
   # Profiles: https://cloud.ibm.com/docs/vpc?topic=vpc-profiles&interface=ui
-  # Originally used cx2-2x4, however 8x16 includes 300G storage.
+  # Uses a storage optimized compute profile
 
   resource_group = var.resource_group
 
