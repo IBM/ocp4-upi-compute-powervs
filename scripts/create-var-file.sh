d02379ff13cfecaa72dfac80e13d4f8b99829067
Paul Bastide <pbastide@us.ibm.com>
2025-04-03 15:19:04 -0400

    fix: OVERRIDE_NETWORK_NAME was improperly identified as empty string
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- scripts/create-var-file.sh
+++ scripts/create-var-file.sh
@@ -175,7 +175,7 @@ if [[ "${FOUND_OVERRIDE}" == "0" ]]
 then
     echo "No override found"
 else
-    export OVERRIDE_NETWORK_NAME="${O_NET_NAME}"
+    export OVERRIDE_NETWORK_NAME="override_network_name=\"${O_NET_NAME}\""
 fi
 
 # creates the var file
@@ -220,7 +220,7 @@ skip_vpc_key = true
 setup_transit_gateway = true
 transit_gateway_name = "multi-arch-x-px-${POWERVS_ZONE}-1-tg"
 
-override_network_name="${OVERRIDE_NETWORK_NAME}"
+${OVERRIDE_NETWORK_NAME}
 EOFXEOF
 }
 
