5a3ebd072af96e293b65e8b9bf097cd71c6c5c2d
Paul Bastide <pbastide@us.ibm.com>
2025-07-01 13:01:06 -0400

    OpenShiftP-347: install nftables prior to use
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- modules/2_pvs_prepare/bastion/bastion.tf
+++ modules/2_pvs_prepare/bastion/bastion.tf
@@ -323,10 +323,10 @@ EOF
   # OpenShiftP-347: add
   provisioner "remote-exec" {
     inline = [<<EOF
+    dnf install -y nftables
 for IFNAME in $(ip --json link show | jq -r '.[] | select(.ifname != "lo").ifname')
 do
     echo "IFNAME: $${IFNAME}"
-    dnf install -y nftables
     nft add table ip nat
     nft 'add chain ip nat prerouting { type nat hook prerouting priority 0 ; }'
     nft 'add chain ip nat postrouting { type nat hook postrouting priority 100 ; }'
